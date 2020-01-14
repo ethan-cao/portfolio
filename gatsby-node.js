671901c614f7e24f0f131a565ab1909be6ad1a9a
ethan-cao <ethancaocgx@gmail.com>
2020-01-14 11:15:21 +0000

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,7 +34,7 @@ exports.createPages = async ({ actions, graphql }) => {
         <a target="_blank" href="https://todo-app-guangxue.herokuapp.com/">React-Redux-TODO</a> --
         <a target="_blank" href="https://github.com/ethan-cao/Todo-React-Redux">Source</a> 
         <br>
-        <a target="_blank" href="">Email-Marketing</a> --
+        <a target="_blank" href="">Email-Marketing (deploying) </a> --
         <a target="_blank" href="https://github.com/ethan-cao/EmailMKT">Source</a> 
         <br><br>
         More is coming...
